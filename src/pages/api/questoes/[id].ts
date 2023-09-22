f23fd6fe2bc54ca7e8c39b9d10e9558799b89409
Wellington Henrique <wellingtonhlc@gmail.com>
2023-09-22 20:42:27 -0300

    124. Banco de Questões

--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -1,7 +1,9 @@
+import questoes from '../bancoDeQuestoes'
+
 export default function handler(req, res) {
-    res.status(200).json({ 
-      id: +req.query.id,
-      name: 'John Doe' 
-    })
-  }
+  res.status(200).json({ 
+    id: +req.query.id,
+    name: 'John Doe' 
+  })
+}
   
