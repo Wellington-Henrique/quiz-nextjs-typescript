2f9f1e45c08fd083b9bb18d7b101c1a35d95ba2f
Wellington Henrique <wellingtonhlc@gmail.com>
2023-09-23 17:17:44 -0300

    141. Componente Botão

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Questao from '../components/Questao'
 import QuestaoModel from '../../model/questao'
 import RespostaModel from '../../model/resposta'
 import { useState } from 'react'
+import Botao from '../components/Botao'
 
 const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostaModel.errada("Verde"),
@@ -26,6 +27,7 @@ export default function Home() {
   return (
     <div style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh'
@@ -35,6 +37,7 @@ export default function Home() {
         respostaFornecida={respostaFornecida}
         tempoEsgotado={tempoEsgotado}
       />
+      <Botao texto="Próxima" href='/resultado'/>
     </div>
   )
 }
