75e08c5ce7a01eee9c548c38e418434e6aed3d60
Wellington Henrique <wellingtonhlc@gmail.com>
2023-09-22 22:52:35 -0300

    126. Construindo a API #01

--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -1,6 +1,13 @@
 import questoes from '../bancoDeQuestoes'
 
 export default function handler(req, res) {
-  res.status(200).json(questoes[0].toEntity())
+  const id = +req.query.id;
+
+  const questao = questoes.find(questao => questao.id === id);
+
+  if (!!questao)
+    res.status(200).json(questao.toEntity())
+  else
+    res.status(204).send("Questão não encontrada!")
 }
   
