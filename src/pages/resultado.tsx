734b1bf7c79f275617c8628c1c9cf42d84d47e28
Wellington Henrique <wellingtonhlc@gmail.com>
2023-09-23 19:43:51 -0300

    149. Componente Resultado

--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+
+import styles from '../styles/Resultado.module.css'
 
 const Resultado = () => {
+  const router = useRouter()
+
+  const total = +router.query.total;
+  const certas = +router.query.certas;
+  const percentual = Math.round((certas/total)*100)
+  
   return (
-    <div>Resultado</div>
+    <div className={styles.resultado}>
+      <h1>Resultado Final</h1>
+      <div>{total}</div>
+      <div>{certas}</div>
+      <div>{`${percentual}%`}</div>
+    </div>
   )
 }
 
